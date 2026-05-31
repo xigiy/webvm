9a27e0195dff63b4e4169c4d22952405d0e86608
xigiy <132143521+xigiy@users.noreply.github.com>
2026-05-31 17:23:11 +0800

    更新 +layout.server.js

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,46 +1,5 @@
-import { parse } from 'node-html-parser';
-import { read } from '$app/server';
-
-var posts = [
-	"https://labs.leaningtech.com/blog/webvm-claude",
-	"https://labs.leaningtech.com/blog/cx-10",
-	"https://labs.leaningtech.com/blog/webvm-20",
-	"https://labs.leaningtech.com/blog/join-the-webvm-hackathon",
-	"https://labs.leaningtech.com/blog/mini-webvm-your-linux-box-from-dockerfile-via-wasm",
-	"https://labs.leaningtech.com/blog/webvm-virtual-machine-with-networking-via-tailscale",
-	"https://labs.leaningtech.com/blog/webvm-server-less-x86-virtual-machines-in-the-browser",
-];
-
-async function getPostData(u)
-{
-	var ret = { title: null, image: null, url: u };
-	var response = await fetch(u);
-	var str = await response.text();
-	var root = parse(str);
-	var tags = root.getElementsByTagName("meta");
-	for(var i=0;i<tags.length;i++)
-	{
-		var metaName = tags[i].getAttribute("property");
-		var metaContent = tags[i].getAttribute("content");
-		switch(metaName)
-		{
-			case "og:title":
-				ret.title = metaContent;
-				break;
-			case "og:image":
-				ret.image = metaContent;
-				break;
-		}
-	}
-	return ret;
-}
-
+// 禁用博客推荐，返回空数组
 export async function load()
 {
-	var ret = [];
-	for(var i=0;i<posts.length;i++)
-	{
-		ret.push(await getPostData(posts[i]));
-	}
-	return { posts: ret };
+	return { posts: [] };
 }
